e11689ae4b1f17e10d09ddc8f987aa562f03d579
Sergio <sergio977@rambler.ru>
2022-11-26 00:33:58 +0400

    add task27 new

--- task27/Program.cs
+++ task27/Program.cs
@@ -6,17 +6,7 @@
 
 9012 -> 12
 */
-/*int sumOfDigits(int start, int finish)
-{
-    int digit1 = 
-    int sum = 0;
-    while ()
-    {
-        sum += i; // sum = sum + i;
-    }
-    return sum;
-}
-*/
+
 int getNumberFromUser(string message)
 {
     int result = 0;
@@ -26,34 +16,14 @@ int getNumberFromUser(string message)
     result = Convert.ToInt32(Console.ReadLine());
     return result;
 }
-for (int i = 0; i < length; i++)
-{
-    int sum = getNumberFromUser % 10;
-    while (Digit < 0)
+    
+int number = getNumberFromUser("Введите число");
+    int sum = number % 10;
+    while (number > 0)
     {
-        Digit = Digit / 10;
-        sum = sum + Digit;
+        number = number / 10;
+        int digit = number % 10;
+        sum = sum + digit;
     }
+Console.WriteLine($"Сумма цифр в числе = {sum}");
 
-}
-
-int userNumber = getNumberFromUser("Введите число");
-int sum = sumOfRange(1, userNumber);
-Console.WriteLine($"Сумма чисел от 1...{userNumber} = {sum}");
-
-int randomNumber = GetRandomNumberInRange(1, 100000);
-Console.WriteLine(randomNumber);
-int threetDigit = randomNumber; 
-if (randomNumber > 100)
-{
-    while (threetDigit > 1000)
-    {
-        threetDigit = threetDigit / 10;
-    }
-int numberthree = threetDigit % 10;
-Console.WriteLine($" {numberthree} ");
-}
-else
-{
-    Console.WriteLine($"Третьей цифры нет");
-}
